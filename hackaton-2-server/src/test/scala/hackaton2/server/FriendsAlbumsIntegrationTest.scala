37d22a882797b1f369dcb3e845fe9d07b5d93365
Kristian Nordal <kristian@mbp.local>
2009-03-29 17:07:44 +0200

    o Added missing param

--- hackaton-2-server/src/test/scala/hackaton2/server/FriendsAlbumsIntegrationTest.scala
+++ hackaton-2-server/src/test/scala/hackaton2/server/FriendsAlbumsIntegrationTest.scala
@@ -7,6 +7,7 @@ import com.jteigen.scalatest.JUnit4Runner
 
 import hackaton2.server.domain._
 
+import java.io.File
 import org.junit.runner.RunWith
 import org.scalatest.{BeforeAndAfter, Spec}
 
@@ -27,7 +28,7 @@ class FriendsAlbumsIntegrationTest extends Spec with BeforeAndAfter {
     Friends ! NewFriend("http://localhost:9090/", "funky")
     Friends ! NewFriend("http://localhost:9091/", "monkey")
     it("send albums to friends") {
-      val replies = Friends !? PostAlbumFriends(FriendsAlbum(MySelf, new Album("Johnny Cash", "Ring of Fire", Nil)))
+      val replies = Friends !? PostAlbumFriends(FriendsAlbum(MySelf, new Album("Johnny Cash", "Ring of Fire", Nil, new File("location"))))
       println("REPLY: " + replies)
       // TODO: Turned off until server clean-up issue resolved
 //      assert(FriendsAlbums !? CountAlbums === 2)
