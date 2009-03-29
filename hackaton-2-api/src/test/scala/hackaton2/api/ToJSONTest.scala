be73cc54620c7ec9fa4410945978c45c2e1cdb71
Jon-Anders <mail@jteigen.com>
2009-03-29 11:35:37 +0200

    fixed bug in jersey provider

--- hackaton-2-api/src/test/scala/hackaton2/api/ToJSONTest.scala
+++ hackaton-2-api/src/test/scala/hackaton2/api/ToJSONTest.scala
@@ -2,20 +2,23 @@ package hackaton.api
 
 
 import com.jteigen.scalatest.JUnit4Runner
-import hackaton2.api.ToJSON
+import hackaton2.api.{FromJSON, ToJSON}
 import org.junit.runner.RunWith
 import org.scalatest.FunSuite
-import util.parsing.json.JSON
 
 @RunWith(classOf[JUnit4Runner])
 class ToJSONTest extends FunSuite {
   test("serializes to and from json"){
     val from = Map("1" -> 1, "2" -> "2", "3" -> List("1", "2", "3"), "4" -> Map("x" -> 1, "y" -> 2))
-    assert(JSON.parseFull(ToJSON(from)).get == from)
+    assert(FromJSON.map(ToJSON(from)) == from)
   }
 
-  test("toJSON"){
+  test("map serializing"){
     val json = ToJSON(Map("url" -> "http://foo.com", "nick" -> "monkey"))
     assert(json === """{"url":"http://foo.com","nick":"monkey"}""")
+  }
+
+  test("handles Nil"){
+    assert(ToJSON(Nil) === "[]")
   }
 }
