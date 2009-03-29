d61b16e475cc48f7bc4b16a6339e01cfb8e78899
Kristian Nordal <kristian@mbp.local>
2009-03-29 12:00:06 +0200

    o Renamed Indexer to FileScanner

--- hackaton-2-indexer/src/test/scala/hackaton/FileScannerTest.scala
+++ hackaton-2-indexer/src/test/scala/hackaton/FileScannerTest.scala
@@ -1,15 +1,17 @@
+package hackaton
+
 import hackaton2.api.ToJSON
 import java.io.File
 import java.net.URL
 import org.junit.Test
 import org.junit.Assert.assertTrue
 
-class IndexerTest {
+class FileScannerTest {
   @Test
   def albums() {
-    val url: URL = getClass.getResource("/music")
-    val music: File = new File(url.toURI)
-    val indexer: Indexer = new Indexer(music.getPath)
+    val url = getClass.getResource("/music")
+    val music = new File(url.toURI)
+    val fileScanner = new FileScanner(music.getPath)
 
     val albums = List[Album](
       Album("TheBand", "TheAlbum", List[Song](
@@ -20,9 +22,9 @@ class IndexerTest {
       Album("OtherBand", "OtherAlbum", List[Song](
         Song("OtherBand", "OtherAlbum", 1, "Song1"),
         Song("OtherBand", "OtherAlbum", 2, "Song2")
-        )),
+        ))
       )
 
-    assertTrue(albums == indexer.albums)
+    assertTrue(albums == fileScanner.albums)
   }
 }
