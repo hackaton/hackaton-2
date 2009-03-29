2f2cb7040f9e8ba2d4e64117f10de7325147df29
Kristian Nordal <kristian@mbp.local>
2009-03-29 12:27:14 +0200

    o Moved Song and Album into their own files (preparing to move them) o Introduced Indexer

--- hackaton-2-indexer/src/main/scala/hackaton/FileScanner.scala
+++ hackaton-2-indexer/src/main/scala/hackaton/FileScanner.scala
@@ -1,6 +1,5 @@
 package hackaton
 
-import hackaton2.api.{ToMap, FromMap}
 import java.io.File
 
 class FileScanner(basedir: String) {
@@ -21,30 +20,4 @@ class FileScanner(basedir: String) {
             val optAlbum = a.get(key).map(_ + song)
             a(key) = optAlbum.getOrElse(Album(song.artist, song.album, List(song)))
   }.values.toList
-}
-
-object Song extends FromMap[Song] {
-  private implicit def toInt(s: String) = Integer.parseInt(s.trim)
-
-  def apply(map: Map[String, Any]) = Song(map.string("artist"), map.string("album"), map.int("trackNo"), map.string("track"))
-
-  def apply(file: File): Song = {
-    val parts = file.getName.substring(0, file.getName.lastIndexOf('.')).split("-").toList
-    Song(parts.first.trim, parts(1).trim, parts(2), parts(3).trim)
-  }
-}
-
-case class Song(artist: String, album: String, trackNo: Int, track: String) extends ToMap {
-  def toMap = Map("artist" -> artist, "album" -> album, "trackNo" -> trackNo, "track" -> track)
-}
-
-object Album extends FromMap[Album] {
-  def apply(map: Map[String, Any]) = Album(map.string("artist"), map.string("name"), map("songs").asInstanceOf[List[Map[String, Any]]].map(Song.apply(_)))
-}
-
-case class Album(artist: String, name: String, songs: List[Song]) extends ToMap {
-  def +(song: Song) =
-    Album(artist, name, song :: songs)
-
-  def toMap = Map("artist" -> artist, "name" -> name, "songs" -> songs.map(_.toMap))
 }
